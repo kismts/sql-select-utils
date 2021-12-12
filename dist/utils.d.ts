f4aec61c62312cf4b9dfcc4a95a795d9b480a972
kismts <kismts@gmail.com>
2021-12-12 04:00:19 +0100

    d.ts file updated

--- dist/utils.d.ts
+++ dist/utils.d.ts
@@ -1,2 +1,2 @@
-export const drawTable: (table: object[], config: object) => string
-export const htmlTable: (table: object[], config: object) => string
+export const drawTable: (table: object[], config?: object) => string
+export const htmlTable: (table: object[], config?: object) => string
